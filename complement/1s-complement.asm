476c666324f545c292c272430da12d32ac8977c3
aryansingh <aryn.snh@gmail.com>
2023-12-19 23:59:23 +0530

    add program complement of a number

---
+++ complement/1s-complement.asm
@@ -0,0 +1,5 @@
+;Program for finding the 1's complement of a number
+LDA 9000H
+CMA
+STA 9001H
+HLT
---
+++ complement/2s-complement.asm
@@ -0,0 +1,6 @@
+;Program for finding the 2's complement of a number
+LDA 9000H
+CMA
+ADI 01H
+STA 9001H
+HLT
